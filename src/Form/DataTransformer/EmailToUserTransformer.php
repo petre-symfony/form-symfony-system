e14a305f192b070477a9c81471c245a0a17e45b4
petrero <petrero@gmail.com>
2020-02-08 17:17:59 +0200

    24.2.Data Transformer{The transform() Method}
    The transform() Method
    
    That's the whole point of transform()! This method is called. when the form is rendering: it takes the raw data for a field - in our case the User object that lives on the author property - and our job is to transform that into a representation that can be used for the form field. In other words, the email string.
    First, if null is the value, just return an empty string. Next, let's add a sanity check: if (! $value instanceof User), then we, the developer, are trying to do something crazy. Throw a new LogicException() that says:
    
      The UserSelectTextType can only be used with User objects.
    Finally, at the bottom, so nice, return $value - which we now know is a User object ->getEmail(). Let's rock! Move over, refresh and.... hello email address!

--- src/Form/DataTransformer/EmailToUserTransformer.php
+++ src/Form/DataTransformer/EmailToUserTransformer.php
@@ -2,12 +2,21 @@
 namespace App\Form\DataTransformer;
 
 
+use App\Entity\User;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class EmailToUserTransformer implements DataTransformerInterface {
 	public function transform($value) {
-		dd('transform', $value);
+		if(null === $value){
+			return '';
+		};
+		
+		if(!$value instanceof User){
+			throw new \LogicException('The UserSelectTextType can only be used with User objects');
+		}
+		
+		return $value->getEmail();
 	}
 	
 	public function reverseTransform($value) {
