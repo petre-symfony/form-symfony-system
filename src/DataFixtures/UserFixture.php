1879679faf11bd0f2ffd5d80d0b4dd5790d75d6c
petrero <petrero@gmail.com>
2020-02-08 12:00:14 +0200

    18.3.Agree to Terms Checkbox Field
    Fixing your Fixtures
    Before we move on, try to reload the fixtures:
    
      php bin/console doctrine:fixtures:load
    It... explodes! Duh! I made the new agreedTermsAt field required in the database, but forgot to update it in the fixtures. No problem: open UserFixture. In the first block, add $user->agreeTerms(). Copy that, and do the same for the admin users.
    Cool! Try it again:
    
      php bin/console doctrine:fixtures:load
    And.... all better!
    Next: let's fix the styling in our registration form by creating our very own form theme.

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -22,6 +22,7 @@ class UserFixture extends BaseFixture {
 			$user = new User();
 			$user->setEmail(sprintf('spacebar%d@example.com', $i));
 			$user->setFirstName($this->faker->firstName);
+			$user->agreeTerms();
 			
 			$user->setPassword(
 				$this->passwordEncoder->encodePassword($user, 'engage')
@@ -43,6 +44,7 @@ class UserFixture extends BaseFixture {
 			if ($this->faker->boolean){
 				$user->setTwitterUsername($this->faker->userName);
 			}
+			$user->agreeTerms();
 			
 			$user->setPassword(
 				$this->passwordEncoder->encodePassword($user, 'engage')
