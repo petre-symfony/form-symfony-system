cb1a47d102780b677324caca8b62b2a03fb173cd
petrero <petrero@gmail.com>
2020-02-08 16:57:32 +0200

    24.1.Data Transformer
    We built a custom field type called UserSelectTextType and we're already using it for the author field. That's cool, except, thanks to getParent(), it's really just a TextType in disguise!
    Internally, TextType basically has no data transformer: it takes whatever value is on the object and tries to print it as the value for the HTML input! For the author field, it means that it's trying to echo that property's value: an entire User object! Thanks to the __toString() method in that class, this prints the first name.
    Let's remove that and see what happens. Refresh! Woohoo! A big ol' error:
    
      Object of class User could not be converted to string
    
    More importantly, even if we put this back, yes, the form would render. But when we submitted it, we would just get a different huge error: the form would try to take the submitted string and pass that to setAuthor().
    To fix this, our field needs a data transformer: something that's capable of taking the User object and rendering its email field. And on submit, transforming that email string back into a User object.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -229,10 +229,6 @@ class User implements UserInterface {
 		return $this;
 	}
 	
-	public function __toString() {
-		return $this->getFirstName();
-	}
-	
 	public function getAgreedTermsAt(): ?\DateTimeInterface{
 		return $this->agrredTermsAt;
 	}
