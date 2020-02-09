18e047be15c61de5ce0c74447e1233eb6022c49c
petrero <petrero@gmail.com>
2020-02-09 17:24:19 +0200

    35.5.Setup: For Dependent Select Fields{Adding the Second Field}
    Adding the Second Field
    Sweet! Back in ArticleFormType, copy the location field, paste, and call it specificLocationName. For the placeholder, use Where exactly?. And for the choices... hmm - this is where things get interesting. I'll just add a dummy "TODO" option to start.
    Back in the form template, copy the location render line, paste it right below, and change it to specificLocationName.
    When we refresh now... no surprise: it works. Here's our location and here's our specificLocationName. But... this is not how we want this to work. When "solar system" is selected, I want this second drop-down to contain a list of planets. If "Near a star" is selected, this should be a list of stars. And if "Interstellar space" is selected, I don't want this field to be in the form at all. Woh.
    The way to solve this is a combination of form events, JavaScript and luck! Ok, I hope we won't need too much of that. Let's start jumping into these topics next!

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -44,6 +44,13 @@ class ArticleFormType extends AbstractType {
 				],
 				'required' => false
 			])
+			->add('specificLocationName', ChoiceType::class, [
+				'placeholder' => 'Where exactly?',
+				'choices' => [
+					'TODO' => 'TODO'
+				],
+				'required' => false
+			])
 		;
 		
 		if ($options['include_published_at']){
