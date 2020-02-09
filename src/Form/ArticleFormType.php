ea6581758c135ac07a296064962e927ca495ced1
petrero <petrero@gmail.com>
2020-02-09 09:23:39 +0200

    32.2.Tweak your Form based on the Underlying Data{Accessing Data via $options}
    Accessing Data via $options
    
    How about: $article = $options['data'] ?? null;
    If you don't know that syntax, it basically says that I want the $article variable to be equal to $options['data'] if it exists and is not null. But if it does not exist, set it to null. Let's dump that and make sure it's what we expect.
    Refresh the new article page - yep - null. Try the edit page... there's the Article object. Now, we are dangerous.

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -21,7 +21,8 @@ class ArticleFormType extends AbstractType {
 	}
 	
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		dd($options);
+		$article = $options['data'] ?? null;
+		dd($article);
 		
 		$builder
 			->add('title', TextType::class, [
